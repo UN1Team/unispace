a143b16875db13621cb9879ce8d5ccf23fb95ae2
AnemonOFF <dimonworld7778@gmail.com>
2021-05-13 19:22:19 +0500

    Test

--- resources/views/console.blade.php
+++ resources/views/console.blade.php
@@ -5,8 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>phpPgConsole</title>
-        <link rel="stylesheet" href="/UniSpace/public/css/console.css">
+        <title>DBConsole</title>
+        <link rel="stylesheet" href="css/console.css">
     </head>
     <body>
         <section class="pastCommands">
@@ -18,6 +18,6 @@
         </section>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
-        <script src="/UniSpace/public/js/console.js"></script>
+        <script src="js/console.js"></script>
     </body>
 </html>
