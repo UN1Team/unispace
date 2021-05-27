9798d82b2d9575c1ef3e3dfabe0ff4fda4878b56
AnemonOFF <dimonworld7778@gmail.com>
2021-05-27 20:02:13 +0500

    Insert info UPDATE and fixes

--- src/app/Http/Controllers/BotController.php
+++ src/app/Http/Controllers/BotController.php
@@ -36,52 +36,138 @@ class BotController extends Controller
         global $vk, $id, $message, $payload, $user_id, $type, $data, $dbUser;
 
         $logsTable = DB::table('logs');
-        $logsTable->insert(['title' => 'Bot', 'text' => 'submit started']);
+        $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - submit started']);
         $vk = vk_api::create(VK_KEY, VERSION)->setConfirm(CONFIRM_STR);
-        $logsTable->insert(['title' => 'Bot', 'text' => 'vk created']);
+        $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - vk created']);
         $vk->debug();
         $vk->initVars($id, $message, $payload, $user_id, $type, $data); //инициализация переменных
-        $logsTable->insert(['title' => 'Bot', 'text' => 'initVars']);
+        $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - initVars']);
 
+        $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - FROM_ID: '.$data->object->from_id]);
         $dbUser = DB::table('vkusers')->WHERE('vkid', $data->object->from_id);
-        $logsTable->insert(['title' => 'Bot', 'text' => 'get user from db']);
+        $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - get user from db']);
 
         if(isset($data->object->action->type) && $data->object->action->type == 'chat_invite_user'){
             $vk->sendMessage($id, "//Вступительный текст для бесед");
         }
         else if(DB::table('users')->where('vkid', $data->object->from_id)->exists()){
+            $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - on stage chooser']);
             $stage = $dbUser->value("stage");
             switch($stage){
                 case "AddOrGet":
-                    ProcessAddOrGetStage();
+                    $this->ProcessAddOrGetStage();
                     break;
                 case "AddInfo":
-                    ProcessAddInfoStage();
+                    $this->ProcessAddInfoStage();
                     break;
                 case "GetInfo":
-                    ProcessGetInfoStage();
+                    $this->ProcessGetInfoStage();
+                    break;
+                case "NewTask":
+                    $this->ProcessNewTaskStage();
+                    break;
+                case "CompleteTask":
+                    $this->ProcessCompleteTaskStage();
+                    break;
+                default:
+                    $vk->sendMessage($id, "Произошла ошибка на сервере, пожалуйста повторите запрос");
+                    $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - ERROR: in switch on stage chooser. Stage was \''.$stage.'\'']);
                     break;
             }
         }
         else{
-            $logsTable->insert(['title' => 'Bot', 'text' => 'go to FIOAuth()']);
-            FIOAuth();
+            $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - go to FIOAuth()']);
+            $this->FIOAuth();
+        }
+    }
+
+    private function ProcessNewTaskStage(){
+        global $vk, $id, $message, $payload, $user_id, $type, $data, $dbUser;
+        
+        $tagsNames = DB::table('users')->where('vkid', $data->object->from_id)->value('tags');
+        $tags = GetTagsByNames($tagsNames);
+
+        if(is_numeric($message) && intval($message) <= count($tagsNames) && !CheckAddInfo($message)){
+            $taskFields = explode('\n');
+            if(count($taskFields) == 2){
+                $id = DB::table('tasks')->insertGetId([
+                    ['title' => $taskFields[0]],
+                    ['description' => $taskFields[1]],
+                    ['deadline' => $taskFields[2]]
+                ]);
+            }
+            else
+            {
+                $id = DB::table('tasks')->insertGetId([
+                    ['title' => $taskFields[0]],
+                    ['description' => $taskFields[1]],
+                    ['deadline' => $taskFields[2]],
+                    ['link' => $taskFields[3]]
+                ]);
+            }
+            $res = DB::table('tags')->where('name', $tagsNames[$message - 1])->value('tasksID');
+            array_push($res, $id);
+            DB::table('tags')->where('name', $tagsNames[$message - 1])->update(['tasksID' => $res]);
+            $vk->sendMessage($id, 'Задание успешно добавлено!');
+            $dbUser->update(['stage' => 'AddOrGet']);
+            $this->ProcessAddOrGetStage();
+        }
+        else
+        {
+            $answer = "Введи номер предмета, по которому ты хочешь добавить задание, описание задания, срок сдачи (гггг-мм-дд) и ссылку (если есть) с разделением в виде переноса строки (shift+enter на ПК)";
+            $counter = 1;
+            foreach($tags as $tag){
+                $answer .= "\n".$counter." - ".$tag->value("nameRU");
+            }
+            $vk->sendMessage($id, $answer);
+        }
+    }
+
+    private function CheckAddInfo(string $info){
+        //TODO: Сделать проверку даты либо её преобразование
+        $rows = explode('\n', $info);
+        return count($rows) >= 2 && count($rows) <= 3;
+    }
+
+    private function ProcessCompleteTaskStage(){
+        global $vk, $id, $message, $payload, $user_id, $type, $data, $dbUser;
+
+        $tagsNames = DB::table('users')->where('vkid', $data->object->from_id)->value('tags');
+        $tasks = array();
+        foreach($tagsNames as $tagName)
+            array_push($tasks, GetTasksByTag($tagName));
+
+        if(is_numeric($message) && intval($message) <= count($tasks))
+        {
+            $res = DB::table('users')->where('vkid', $data->object->from_id)->value('completedTasksID');
+            array_push($res, $message);
+            DB::table('users')->where('vkid', $data->object->from_id)->update(['completedTasksID' => $res]);
+            $vk->sendMessage('Задание выполнено, поздравляем!');
+            $dbUser->update(['stage' => 'AddOrGet']);
+            $this->ProcessAddOrGetStage();
+        }
+        else
+        {
+            $answer = "Введи номер задания:";
+            $counter = 1;
+            foreach($tasks as $task){
+                $answer .= "\n".$counter." - ".$tasks;
+            }
+            $vk->sendMessage($id, $answer);
         }
     }
 
     private function ProcessAddInfoStage(){
         global $vk, $id, $message, $payload, $user_id, $type, $data, $dbUser;
 
-        $vk->sendMessage($id, 'Данный раздел ещё в разработке');
-        $dbUser->update(['stage' => 'AddOrGet']);
-        die();
-
         if($payload){
             if($command == 'CompleteTask'){
                 $dbUser->update(['stage' => 'CompleteTask']);
+                $this->ProcessCompleteTaskStage();
             }
             else if ($command == 'NewTask'){
                 $dbUser->update(['stage' => 'NewTask']);
+                $this->ProcessNewTaskStage();
             }
         } else
         {
@@ -106,9 +192,10 @@ class BotController extends Controller
             else
                 array_push($tagNumber, intval($message));
 
-            SendTasksByTagsNumber($tagsNumber, $tagsNames);
+            $this->SendTasksByTagsNumberWithoutCompleted($tagsNumber, $tagsNames);
             $dbUser->update(['stage' => 'AddOrGet']);
-        } 
+            $this->ProcessAddOrGetStage();
+        }
         else {
             $answer = "Теперь давай решим, что ты хочешь узнать (просто введи цифру предмета):\n0 - Все";
             $counter = 1;
@@ -119,9 +206,10 @@ class BotController extends Controller
         }
     }
 
-    private function SendTasksByTagsNumber($tagsNumber, $tagsNames){
-        global $vk;
+    private function SendTasksByTagsNumberWithoutCompleted($tagsNumber, $tagsNames){
+        global $vk, $id, $message, $payload, $user_id, $type, $data, $dbUser;
 
+        //TODO: добавить проверку на completed в таблице users
         $dbTags = DB::table('tags');
         foreach($tagsNumber as $tagNumber){
             $tagName = $tagsNames[$tagNumber - 1];
@@ -129,6 +217,14 @@ class BotController extends Controller
             foreach($tasksID as $taskID)
                 $vk->sendMessage($id, TaskToString($taskID));
         }
+    }
+
+    private function GetTasksByTag(string $tag){
+        $tasksID = DB::table('tags')->where('name', $tag)->value('tasksID');
+        $result = array();
+        foreach($tasksID as $taskID)
+            array_push($result, TaskToString($taskID));
+        return $tasksID;
     }
 
     private function TaskToString($taskID){
@@ -142,6 +238,7 @@ class BotController extends Controller
         foreach($tagsNames as $tagName){
             array_push($tags, $dbTags->where('name', $tagName));
         }
+        return $tags;
     }
 
     private function ProcessAddOrGetStage(){
@@ -150,11 +247,11 @@ class BotController extends Controller
         if($payload){
             if($command == 'AddInfo'){
                 $dbUser->update(['stage' => "AddInfo"]);
-                ProcessAddInfoStage();
+                $this->ProcessAddInfoStage();
             }
             else if ($command == 'GetInfo'){
                 $dbUser->update(['stage' => "GetInfo"]);
-                ProcessGetInfoStage();
+                $this->ProcessGetInfoStage();
             }
         }
         else {
@@ -168,7 +265,7 @@ class BotController extends Controller
         global $vk, $id, $message, $payload, $user_id, $type, $data, $dbUser;
 
         $logsTable = DB::table('logs');
-        $logsTable->insert(['title' => 'Bot', 'text' => 'FIOAuth()']);
+        $logsTable->insert(['title' => 'Bot', date("m.d.y H:m:s").' - text' => 'FIOAuth()']);
         if($payload){
             if($command == 'yesFio'){
                 $currentFio = $dbUser->value('tempFio');
@@ -176,7 +273,7 @@ class BotController extends Controller
                     $fio = explode(' ', $currentFio);
                     DB::table('users')->insert(['name' => $fio[0], 'last' => $fio[1], 'vkid' => $data->object->from_id]);
                     $dbUser->update(['stage' => "AddOrGet"]);
-                    ProcessAddOrGetStage();
+                    $this->ProcessAddOrGetStage();
                 }
                 else
                     $vk->sendMessage($id, "Введи пожалуйста Имя и Фамилию правильно. Первым - имя, вторым фамилию, через пробел, русскими буквами.");
@@ -191,9 +288,9 @@ class BotController extends Controller
             $dbUser->update(['tempFio' => $message]);
         }
         else {
-            $logsTable->insert(['title' => 'Bot', 'text' => 'else statement before sendMessage in FIOAuth()']);
+            $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - else statement before sendMessage in FIOAuth()']);
             $vk->sendMessage($id, "Привет!\n\nЯ твой персональный бот-помощник по учёбе.\nЯ буду тебя информировать о заданиях с их сроками сдачи и о том, что творится в тоей учебной жизни!\n\nХочешь узнать подробнее?\nНапиши своё Имя и Фамилию.");
-            $logsTable->insert(['title' => 'Bot', 'text' => 'message sent']);
+            $logsTable->insert(['title' => 'Bot', 'text' => date("m.d.y H:m:s").' - message sent']);
             $dbUser->update(['stage' => "Fio"]);
         }
     }
